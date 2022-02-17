2f8af06e96610d437e34a0eb02a53e5bea6c0255
hazhio <99158882+hazhio@users.noreply.github.com>
2022-02-17 23:24:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -228,3 +228,20 @@ const CONFIG = {
     },
   ],
 };
+
+let searchBox=document.getElementById("searchtxt");
+
+
+searchBox.addEventListener('keypress',(e)=>{
+    if(e.keyCode==13){
+        setTimeout(()=>{searchBox.value=""},1000);
+    }
+});
+
+
+
+searchBox.addEventListener('keydown',(e)=>{
+    if(searchBox.value=="f" && e.key=="b"){
+        window.open('https://www.facebook.com',"_self");
+    }
+})
